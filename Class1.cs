c272ca95dd4732938a06fad5ef3295f8337e4a37
wenyidaMAC <wenyidaMAC@DESKTOP-FAL972E>
2021-08-16 01:15:32 +0800

    添加项目文件。

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace JianYingPluse
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Class1
+    {
+        public string getProjectPath(string JsionPaht)
+        {
+            string 
+            return JsionPaht;
+        }
+    }
+}
